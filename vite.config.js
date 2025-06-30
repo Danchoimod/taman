322abb0ed21ec4b248db1fbcdf577c44b57c4d2c
PhuPham <tranphupham2909@gmail.com>
2025-06-30 23:31:42 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/taman/', // 👈 thêm dòng này
+  base: '/', // 👈 thêm dòng này/taman/
   plugins: [react()],
 })
