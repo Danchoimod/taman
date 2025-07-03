feb14c783b91c48a7f575fcbcfc4240f4af0ca5c
PhuPham <tranphupham2909@gmail.com>
2025-07-04 02:14:07 +0700

    rin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/taman/', // 👈 thêm dòng này/taman/ nếu dùng github
+  base: '/', // 👈 thêm dòng này/taman/ nếu dùng github
   plugins: [react()],
 })
