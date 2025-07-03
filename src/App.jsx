4e54671e834a4d59f360f5f1ba70bdc8c02993a6
PhuPham <tranphupham2909@gmail.com>
2025-07-04 01:14:33 +0700

    run

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Branch from './pages/BranchSelector'
 import RoomBooking from './pages/RoomBooking'
 
