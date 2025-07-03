31ced320846f70ec7fc6ecdf4611a6319fe0f70f
PhuPham <tranphupham2909@gmail.com>
2025-07-04 01:33:16 +0700

    fix: correct import path for case-sensitive build

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Branch from './pages/BranchSelector'
 import RoomBooking from './pages/RoomBooking'
-import Home from './pages/home'; // ✅ Chính xác 100%
+import Home from './pages/home';
+
 
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/branch" element={<Branch />} />
       <Route path="/booking/:branchId" element={<RoomBooking />} /> {/* ✅ Route có params */}  
     </Routes>
