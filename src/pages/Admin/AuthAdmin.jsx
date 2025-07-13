c4e863ba8c3249aca74e5adc31f40ff90d916b5f
PhuPham <tranphupham2909@gmail.com>
2025-07-14 03:09:22 +0700

    run

--- src/pages/Admin/AuthAdmin.jsx
+++ src/pages/Admin/AuthAdmin.jsx
@@ -6,7 +6,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useNavigate } from 'react-router-dom';
 const supabase = createClient('https://xodhvzvlgwrzrdrnbzev.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvZGh2enZsZ3dyenJkcm5iemV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzMjkyNTYsImV4cCI6MjA2NjkwNTI1Nn0.zNtwvH1fNH-hc6iCelhdOYgaANpnKaLjYK-OpNG4tqA')
 
-export default function AuthPage() {
+export default function AuthAdmin() {
   const [session, setSession] = useState(null)
   const navigate = useNavigate();
 
